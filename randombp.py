d8c8c050d6e7368943109690e1454efb7fc78a9c
Lisa Zhang <lisa@polychart.com>
2017-03-06 20:07:34 -0500

    Add (non-working) 4-layer FA net; add weight decay

--- randombp.py
+++ randombp.py
@@ -21,7 +21,7 @@ def wi(name, shape, dtype=tf.float32):
         shape=shape,
         dtype=dtype,
         initializer=tf.truncated_normal_initializer(
-          mean=0.0, stddev=1 / np.sqrt(np.prod(shape[:-1])), dtype=dtype))
+          mean=0.0, stddev=0.01, dtype=dtype))
 
 
 class BackPropNet(object):
@@ -44,7 +44,10 @@ class BackPropNet(object):
     def define_placeholders(self):
         self.x = tf.placeholder(tf.float32, [None, 784])
         self.y = tf.placeholder(tf.float32, [None, 10])
-        self.lr = tf.placeholder(tf.float32)
+        self.lr = tf.placeholder(tf.float32) # learning rate
+        self.decay = tf.placeholder(tf.float32) # weight decay
+        self.N = tf.cast(tf.shape(self.x)[0], tf.float32)
+        self.alpha = self.lr # / self.N
 
     def define_costs(self):
         # cross-entropy
@@ -54,11 +57,15 @@ class BackPropNet(object):
         correct_prediction = tf.equal(tf.argmax(self.ypred,1), tf.argmax(self.y,1))
         self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-    def train(self, sess, data, lr=0.5, iter=100):
+    def train(self, sess, data, lr=0.5, decay=0.0, iter=50):
         for i in range(iter):
             images, labels = data.next_batch(1000)
             _, acc = sess.run([self.train_step, self.accuracy],
-                    feed_dict={self.x: images, self.y: labels, self.lr: lr})
+                    feed_dict={self.x: images,
+                               self.y: labels,
+                               self.lr: lr,
+                               self.decay: decay
+                               })
             print 'Iter %d, Acc: %.1f %%' % (i, acc*100)
 
 class RandomFeedbackNet(BackPropNet):
@@ -105,11 +112,78 @@ class RandomFeedbackNet(BackPropNet):
         self.d1_grad = tf.reduce_sum(z1_grad, [0])
         # training: assign weights
         self.train_step= [
-            tf.assign(w2, w2 - self.lr * self.w2_grad),
-            tf.assign(w1, w1 - self.lr * self.w1_grad),
-            tf.assign(d2, d2 - self.lr * self.d2_grad),
-            tf.assign(d1, d1 - self.lr * self.d1_grad),
+            tf.assign(w2, w2 - self.alpha * self.w2_grad - self.decay),
+            tf.assign(w1, w1 - self.alpha * self.w1_grad - self.decay),
+            tf.assign(d2, d2 - self.alpha * self.d2_grad - self.decay),
+            tf.assign(d1, d1 - self.alpha * self.d1_grad - self.decay),
         ]
+
+class RandomFeedback4Layer(RandomFeedbackNet):
+    """A 4-layer network, as above
+    """
+    def __init__(self, num_hidden=[300, 100]):
+        self.define_placeholders()
+        # define network:
+        n1, n2 = num_hidden
+        w1 = wi("fa_w1", [784, n1]) # forward weights
+        d1 = bi("fa_d1", [n1])
+        z1 = tf.matmul(self.x, w1) + d1
+        h1 = tf.nn.sigmoid(z1)
+        w2 = wi("fa_w2", [n1, n2]) # forward weights
+        b2 = wi("fa_b2", [n2, n1]) # backwards weights
+        d2 = bi("fa_d2", [n2])
+        z2 = tf.matmul(h1, w2) + d2
+        h2 = tf.nn.sigmoid(z2)
+        w3 = wi("fa_w3", [n2, 10]) # forward weights
+        b3 = wi("fa_b3", [10, n2]) # backwards weights
+        d3 = bi("fa_d3", [10])
+        z3 = tf.matmul(h2, w3) + d3
+        self.ypred = tf.nn.softmax(z3)
+        # costs
+        self.define_costs()
+
+        # training: derivative w.r.t. activations
+        ypred_grad = tf.gradients(self.cross_entropy, self.ypred)[0]
+        z3_grad = tf.gradients(self.cross_entropy, z3)[0] # softmax
+        h2_grad = tf.matmul(z3_grad, b3)
+        z2_grad = tf.mul(tf.gradients(h2, z2)[0], h2_grad) #sigmoid
+        h1_grad = tf.matmul(z2_grad, b2)
+        z1_grad = tf.mul(tf.gradients(h1, z1)[0], h1_grad) #sigmoid
+
+        # training: derivative w.r.t. weights
+        self.w3_grad = tf.reduce_sum(
+                tf.mul(tf.expand_dims(h2, 2),
+                       tf.expand_dims(z3_grad, 1)),
+                [0])
+        self.d3_grad = tf.reduce_sum(z3_grad, [0])
+        self.w2_grad = tf.reduce_sum(
+                tf.mul(tf.expand_dims(h1, 2),
+                       tf.expand_dims(z2_grad, 1)),
+                [0])
+        self.d2_grad = tf.reduce_sum(z2_grad, [0])
+        self.w1_grad = tf.reduce_sum(
+                tf.mul(tf.expand_dims(self.x, 2),
+                       tf.expand_dims(z1_grad, 1)),
+                [0])
+        self.d1_grad = tf.reduce_sum(z1_grad, [0])
+        # training: assign weights
+        self.train_step= [
+            tf.assign(w3, w3 - self.alpha * self.w3_grad - self.decay),
+            tf.assign(w2, w2 - self.alpha * self.w2_grad - self.decay),
+            tf.assign(w1, w1 - self.alpha * self.w1_grad - self.decay),
+            tf.assign(d3, d3 - self.alpha * self.d3_grad - self.decay),
+            tf.assign(d2, d2 - self.alpha * self.d2_grad - self.decay),
+            tf.assign(d1, d1 - self.alpha * self.d1_grad - self.decay),
+        ]
+
+class DirectFeedbackNet(BackPropNet):
+    """Based on
+        'Direct Feedback Alignment Provides Learning in
+        Deep Neural Networks'
+    There are going to be some differences. I'll know later.
+    """
+    def __init__(self, num_hidden=100):
+        pass
 
 
 if __name__ == '__main__':
@@ -118,10 +192,14 @@ if __name__ == '__main__':
 
     bpn = BackPropNet()
     rfn = RandomFeedbackNet()
+    rfn4 = RandomFeedback4Layer([100,100])
     with tf.Session() as sess:
         sess.run(tf.initialize_all_variables())
-        print 'Training normal neural net with backprop:'
-        bpn.train(sess, mnist.train)
-        print 'Training random feedback neural net:'
-        rfn.train(sess, mnist.train, 0.5)
+        #print 'Training normal neural net with backprop:'
+        #bpn.train(sess, mnist.train)
 
+        #print 'Training random feedback neural net:'
+        #rfn.train(sess, mnist.train, lr=0.5, decay=0.00001, iter=30)
+
+        print 'Training 4-layer random feedback net:'
+        rfn4.train(sess, mnist.train, lr=0.005, decay=0.00001, iter=30)

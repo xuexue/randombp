eb52b5b66a6507d0e36accf0afc7b1f493c99868
Lisa Zhang <lczhang@cs.toronto.edu>
2017-03-06 21:13:12 -0500

    Add direct feedback net

--- randombp.py
+++ randombp.py
@@ -182,9 +182,60 @@ class DirectFeedbackNet(BackPropNet):
         Deep Neural Networks'
     There are going to be some differences. I'll know later.
     """
-    def __init__(self, num_hidden=100):
-        pass
+    def __init__(self, num_hidden=[300, 100]):
+        self.define_placeholders()
+        # define network:
+        n1, n2 = num_hidden
+        w1 = wi("df_w1", [784, n1]) # forward weights
+        d1 = bi("df_d1", [n1])
+        z1 = tf.matmul(self.x, w1) + d1
+        h1 = tf.nn.sigmoid(z1)
+        w2 = wi("df_w2", [n1, n2]) # forward weights
+        b2 = wi("df_b2", [10, n1]) # backwards weights <--- SUBTLE DIFFERENCE
+        d2 = bi("df_d2", [n2])
+        z2 = tf.matmul(h1, w2) + d2
+        h2 = tf.nn.sigmoid(z2)
+        w3 = wi("df_w3", [n2, 10]) # forward weights
+        b3 = wi("df_b3", [10, n2]) # backwards weights
+        d3 = bi("df_d3", [10])
+        z3 = tf.matmul(h2, w3) + d3
+        self.ypred = tf.nn.softmax(z3)
+        # costs
+        self.define_costs()
 
+        # training: derivative w.r.t. activations
+        ypred_grad = tf.gradients(self.cross_entropy, self.ypred)[0]
+        z3_grad = tf.gradients(self.cross_entropy, z3)[0] # softmax
+        h2_grad = tf.matmul(z3_grad, b3)
+        z2_grad = tf.multiply(tf.gradients(h2, z2)[0], h2_grad) #sigmoid
+        h1_grad = tf.matmul(z3_grad, b2) # <--- SUBTLE DIFFERENCE HERE
+        z1_grad = tf.multiply(tf.gradients(h1, z1)[0], h1_grad) #sigmoid
+
+        # training: derivative w.r.t. weights
+        self.w3_grad = tf.reduce_sum(
+                tf.multiply(tf.expand_dims(h2, 2),
+                       tf.expand_dims(z3_grad, 1)),
+                [0])
+        self.d3_grad = tf.reduce_sum(z3_grad, [0])
+        self.w2_grad = tf.reduce_sum(
+                tf.multiply(tf.expand_dims(h1, 2),
+                       tf.expand_dims(z2_grad, 1)),
+                [0])
+        self.d2_grad = tf.reduce_sum(z2_grad, [0])
+        self.w1_grad = tf.reduce_sum(
+                tf.multiply(tf.expand_dims(self.x, 2),
+                       tf.expand_dims(z1_grad, 1)),
+                [0])
+        self.d1_grad = tf.reduce_sum(z1_grad, [0])
+        # training: assign weights
+        self.train_step= [
+            tf.assign(w3, w3 - self.alpha * self.w3_grad - self.decay),
+            tf.assign(w2, w2 - self.alpha * self.w2_grad - self.decay),
+            tf.assign(w1, w1 - self.alpha * self.w1_grad - self.decay),
+            tf.assign(d3, d3 - self.alpha * self.d3_grad - self.decay),
+            tf.assign(d2, d2 - self.alpha * self.d2_grad - self.decay),
+            tf.assign(d1, d1 - self.alpha * self.d1_grad - self.decay),
+        ]
 
 if __name__ == '__main__':
     from tensorflow.examples.tutorials.mnist import input_data
@@ -193,14 +244,20 @@ if __name__ == '__main__':
     bpn = BackPropNet()
     rfn = RandomFeedbackNet()
     rfn4 = RandomFeedback4Layer([200,100])
+    dfb4 = DirectFeedbackNet([200,100])
     with tf.Session() as sess:
         sess.run(tf.initialize_all_variables())
 
+        '''
         print 'Training normal neural net with backprop:'
-        bpn.train(sess, mnist.train, iter=30)
+        bpn.train(sess, mnist.train, iter=1000)
 
         print 'Training random feedback neural net:'
-        rfn.train(sess, mnist.train, lr=0.5, decay=0.00001, iter=50)
+        rfn.train(sess, mnist.train, lr=0.5, decay=0.00001, iter=1000)
 
         print 'Training 4-layer random feedback net:'
-        rfn4.train(sess, mnist.train, lr=0.5, decay=0.0001, iter=50)
+        rfn4.train(sess, mnist.train, lr=0.5, decay=0.0001, iter=1000)
+        '''
+
+        print 'Training 4-layer direct feedback net:'
+        dfb4.train(sess, mnist.train, lr=0.5, decay=0.0001, iter=1000)
